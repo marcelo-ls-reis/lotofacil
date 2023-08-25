b3a8da3f0e47b74d0cfa7b6dfd33edb30b949a83
marcelo-ls-reis <marcelo.reis5@fatec.sp.gov.br>
2023-08-25 13:58:07 -0300

    tratamento de arquivo

--- tratar.py
+++ tratar.py
@@ -40,7 +40,7 @@
 #     print(arquivo) 
 # arquivo.close()       
 
-arquivo = open("Resultados.txt","r")
+""" arquivo = open("Resultados.txt","r")
 linha = arquivo.readline()
 lista = []
 while linha:
@@ -53,4 +53,59 @@ for i in lista:
     arquivo.close()
     
 
-print(lista)
+print(lista) """
+
+
+""" def substituir_espacos_por_virgulas(arquivo):
+    # Abrir e ler o arquivo
+    with open(arquivo, 'r') as f:
+        conteudo = f.read()
+        print(conteudo)
+
+    # Modificar o conteúdo
+    conteudo_modificado = conteudo.replace(" ", ",")
+    print(conteudo_modificado)
+
+    # Salvar as modificações
+    with open(arquivo, 'w') as f:
+        f.write(conteudo_modificado)
+
+# Uso
+arquivo = 'resultados_novos.txt'
+substituir_espacos_por_virgulas(arquivo) """
+
+""" import re
+
+def substituir_espacos_por_virgulas(arquivo):
+    # Abrir e ler o arquivo
+    with open(arquivo, 'r') as f:
+        conteudo = f.read()
+        print(conteudo)
+
+    # Modificar o conteúdo usando regex para substituir sequências de espaços ou tabulações por vírgulas
+    conteudo_modificado = re.sub(r'  +', ',', conteudo)
+    print(conteudo_modificado)
+
+    # Salvar as modificações
+    with open(arquivo, 'w') as f:
+        f.write(conteudo_modificado)
+
+# Uso
+arquivo = 'resultados_novos.txt'
+substituir_espacos_por_virgulas(arquivo)
+
+ """
+
+def substituir_tabs_por_virgulas(arquivo):
+    # Ler o arquivo linha por linha e substituir as tabulações
+    with open(arquivo, 'r') as f:
+        linhas = [linha.replace("\t", ",") for linha in f.readlines()]
+
+    # Salvar as modificações
+    with open(arquivo, 'w') as f:
+        f.write(''.join(linhas))
+
+# Uso
+nome_do_arquivo = 'resultados_novos.txt'
+substituir_tabs_por_virgulas(nome_do_arquivo)
+
